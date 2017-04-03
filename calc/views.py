9891c10438ebcfbcedbb76ee315408aa5634e0df
Onac8 <canopicazojonathan@gmail.com>
2017-04-03 09:20:32 +0200

    Calculadora simple en Django

--- calc/views.py
+++ calc/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import HttpResponseBadRequest
 
-# Create your views here.
+def sum(request, op1, op, op2):
+    resultado = int(op1) + int(op2)
+    htmlAnswer = "<p> The operation: " + op1 + " " + op \
+                + " " + op2 + " = " + str(resultado) + "</p>"
+    return HttpResponse(htmlAnswer)
+
+def mul(request, op1, op, op2):
+    resultado = int(op1) * int(op2)
+    htmlAnswer = "<p> The operation: " + op1 + " " + op \
+                + " " + op2 + " = " + str(resultado) + "</p>"
+    return HttpResponse(htmlAnswer)
+
+def div(request, op1, op, op2):
+    try:
+        resultado = int(op1) / int(op2)
+        htmlAnswer = "<p> The operation: " + op1 + " " + op \
+                    + " " + op2 + " = " + str(resultado) + "</p>"
+        return HttpResponse(htmlAnswer)
+    except ZeroDivisionError:
+        htmlAnswer = "<p> You tried to divide by zero... </p>"
+        return HttpResponseBadRequest(htmlAnswer)
+
+def sub(request, op1, operacion, op2):
+        resultado = int(op1) - int(op2)
+        htmlAnswer = "<p> El resultado--> " + op1 + " " + op \
+                    + " " + op2 + " = " + str(resultado) + "</p>"
+        return HttpResponse(htmlAnswer)
--- project/urls.py
+++ project/urls.py
@@ -2,9 +2,9 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'project.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+    url(r'(\d+)(\+)(\d+)','calc.views.sum',),
+    url(r'(\d+)(\*)(\d+)','calc.views.mul',),
+    url(r'(\d+)(\/)(\d+)','calc.views.div',),
+    url(r'(\d+)(-)(\d+)','calc.views.sub',),
     url(r'^admin/', include(admin.site.urls)),
 )
